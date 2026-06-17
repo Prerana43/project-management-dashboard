e61ba5681dbed40cdfdcfbbd625c64663666baca
Prerana Nishad <prerananishad6@gmail.com>
2026-06-17 14:51:21 +0530

    Connected Backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    "https://project-management-dashboard-bqvv.onrender.com",
 });
 
 API.interceptors.request.use(
